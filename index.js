74f95e92ca2acbb1715bf3c79e96f24040b05c4f
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-08-31 19:32:37 +0700

    setup project structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+io.on('connection', socket => {
+    socket.emit("yourID", socket.id);
+});
+
+server.listen(5000, () => console.log('server is running on port 5000'));
