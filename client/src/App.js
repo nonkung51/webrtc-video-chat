36953056b0925c7ec40e650b2f493328215091bc
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-08-31 22:16:03 +0700

    Add list of online people

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,33 @@ const Container = styled.div`
 	align-items: center;
 `;
 
+const CallerContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+`;
+
+const CallButton = styled.button`
+	margin: 1rem;
+	padding: 0.8rem;
+	border-radius: 2rem;
+	border-width: 0rem;
+
+	&:hover {
+		background-color: gray;
+	}
+
+	&:focus {
+		outline: none;
+	}
+`;
+
 function App() {
 	const socket = useRef();
 	const [stream, setStream] = useState();
 	const userVideo = useRef();
-	
+	const [myID, setMyID] = useState();
+	const [onlineUsers, setOnlineUsersList] = useState({});
 
 	useEffect(() => {
 		socket.current = io.connect('localhost:5000');
@@ -31,6 +53,12 @@ function App() {
 					userVideo.current.srcObject = stream;
 				}
 			});
+		socket.current.on('id_report', (id) => {
+			setMyID(id);
+		});
+		socket.current.on('online_users_report', (users) => {
+			setOnlineUsersList(users);
+		});
 	}, []);
 
 	let UserVideo;
@@ -40,8 +68,15 @@ function App() {
 
 	return (
 		<Container>
-			{ UserVideo }
+			{UserVideo}
 			<h1>Hey!</h1>
+			<CallerContainer>
+				{Object.values(onlineUsers).map((user) => {
+					if (user.id !== myID) {
+						return <CallButton>{`Call ${user.id}`}</CallButton>;
+					}
+				})}
+			</CallerContainer>
 		</Container>
 	);
 }
--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
 
+const users = {};
+
 io.on('connection', socket => {
-    socket.emit("yourID", socket.id);
+    if (!users[socket.id]) {
+        users[socket.id] = {id: socket.id};
+    }
+    socket.emit("id_report", socket.id);
+    io.sockets.emit("online_users_report", users);
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+        io.sockets.emit("online_users_report", users);
+    })
 });
 
 server.listen(5000, () => console.log('server is running on port 5000'));
